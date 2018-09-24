b4f7f46eec3ccb3018a370cd98f1a4a3f5883dd5
Felix Deimel <felix@lemonmojo.com>
2018-09-24 17:18:04 +0200

    Initial commit

---
+++ MonoOnMojaveSimpleXam/AppDelegate.cs
@@ -0,0 +1,21 @@
+﻿using AppKit;
+using Foundation;
+
+namespace MonoOnMojaveSimpleXam
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : NSApplicationDelegate
+	{
+		public override void DidFinishLaunching(NSNotification notification)
+		{
+			NSAlert alert = new NSAlert() {
+				MessageText = "Message",
+				InformativeText = "Informative"
+			};
+
+			alert.AddButton("OK");
+
+			alert.RunModal();
+		}
+	}
+}
---
+++ MonoOnMojaveSimpleXam/Main.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+using AppKit;
+
+namespace MonoOnMojaveSimpleXam
+{
+	static class MainClass
+	{
+		[DllImport("/System/Library/Frameworks/Cocoa.framework/Cocoa", EntryPoint = "NSApplicationLoad")]
+		static extern bool NSApplicationLoad();
+
+		static void Main(string[] args)
+		{
+			// This call fixes the strangeness !!!
+			//NSApplicationLoad();
+
+			NSApplication.Init();
+			NSApplication.Main(args);
+		}
+	}
+}
